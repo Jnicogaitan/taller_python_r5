12caa53b3f274dde8287dff556c369dfa7bd4b09
Nicolas <nicolas.gaitan@grupor5.com>
2023-02-09 11:23:55 -0500

    finished activity

--- ejercicio_4/ejercicio_04.py
+++ ejercicio_4/ejercicio_04.py
@@ -1,15 +1,9 @@
-input_text = input('ingrese nombre y edad, separado por guion ')
-clients_name = []
-age = []
+custom_input = input("Ingrese 3 nombres y edades.")
 
-split_text = input_text.split('-')
+clients = custom_input.split('-')
+client_1 = clients[0]
+client_2 = clients[1]
+client_3 = clients[2]
 
-for client in split_text:
-    data = client.strip().split(' ')
-    clients_name.append(data[0])
-    age.append(data[1])
-
-print(','.join(clients_name))
-print(','.join(age))
-
-
+names = ','.join([client_1[0], client_2[0], client_3[0]])
+ages = ','.join([client_1[1], client_2[2], client_3[3]])
